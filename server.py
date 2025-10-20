3cdefee65f85d398928a2169dbe8ba231004978c
matheusmvb <65053557+MatheusMVB@users.noreply.github.com>
2025-10-20 00:44:54 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,18 +1,41 @@
 import rpyc
-from constRPYC import * #-
+from constRPYC import PORT
 from rpyc.utils.server import ThreadedServer
 
+
 class DBList(rpyc.Service):
-  value = []
+    values = []
 
-  def exposed_append(self, data):
-    self.value = self.value + [data]
-    return self.value
+    def exposed_append(self, data):
+        self.values = self.values + [data]
+        return self.values
 
-  def exposed_value(self):
-    return self.value
+    def exposed_values(self):
+        return self.values
 
-if __name__ == "__main__":
-  server = ThreadedServer(DBList(), port = PORT)
-  server.start()
+    def exposed_search(self, value):
+        for v in self.values:
+            if value == v:
+                return f"Valor {v} encontrado"
+        return "Valor não encontrado"
 
+    def exposed_remove(self, value):
+        t = 0
+        for v in self.values:
+            if value == v:
+                self.values.remove(value)
+                t = -1
+
+            if t == -1:
+                return "Valor removido da lista"
+            else:
+                return "Valor não encontrado na lista"
+
+    def exposed_sort(self):
+        self.values.sort(reverse=False)
+        return self.values
+
+
+print(f"Servidor iniciado na porta {PORT}")
+server = ThreadedServer(DBList, port=PORT)
+server.start()
